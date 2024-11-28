64344cc65f5a2641dfb2d0c72ed3aa5fa95b26db
Matthew Croall <matthew.croall@icloud.com>
2024-11-28 19:44:45 +1030

    Add podman prune support

--- lib/kamal/commands/prune.rb
+++ lib/kamal/commands/prune.rb
@@ -3,21 +3,21 @@ require "active_support/core_ext/numeric/time"
 
 class Kamal::Commands::Prune < Kamal::Commands::Base
   def dangling_images
-    docker :image, :prune, "--force", "--filter", "label=service=#{config.service}"
+    container_manager :image, :prune, "--force", "--filter", "label=service=#{config.service}"
   end
 
   def tagged_images
     pipe \
-      docker(:image, :ls, *service_filter, "--format", "'{{.ID}} {{.Repository}}:{{.Tag}}'"),
+      container_manager(:image, :ls, *service_filter, "--format", "'{{.ID}} {{.Repository}}:{{.Tag}}'"),
       grep("-v -w \"#{active_image_list}\""),
-      "while read image tag; do docker rmi $tag; done"
+      "while read image tag; do #{container_manager_cmd} rmi $tag; done"
   end
 
   def app_containers(retain:)
     pipe \
-      docker(:ps, "-q", "-a", *service_filter, *stopped_containers_filters),
+      container_manager(:ps, "-q", "-a", *service_filter, *stopped_containers_filters),
       "tail -n +#{retain + 1}",
-      "while read container_id; do docker rm $container_id; done"
+      "while read container_id; do #{container_manager_cmd} rm $container_id; done"
   end
 
   private
@@ -29,10 +29,14 @@ class Kamal::Commands::Prune < Kamal::Commands::Base
       # Pull the images that are used by any containers
       # Append repo:latest - to avoid deleting the latest tag
       # Append repo:<none> - to avoid deleting dangling images that are in use. Unused dangling images are deleted separately
-      "$(docker container ls -a --format '{{.Image}}\\|' --filter label=service=#{config.service} | tr -d '\\n')#{config.latest_image}\\|#{config.repository}:<none>"
+      "$(#{container_manager_cmd} container ls -a --format '{{.Image}}\\|' --filter label=service=#{config.service} | tr -d '\\n')#{config.latest_image}\\|#{config.repository}:<none>"
     end
 
     def service_filter
       [ "--filter", "label=service=#{config.service}" ]
+    end
+
+    def container_manager_cmd
+      config.container_manager.manager
     end
 end
